89170b4fa0f5c2a85d4b7eba5abd585eae5cbc5e
SK AFTAB <akaftabsqalbi0820@gmail.com>
2025-04-10 16:00:45 +0530

    Update game.py

--- game.py
+++ game.py
@@ -6,8 +6,8 @@ st.write("You Have Only 5 Attempt !")
 
 if 'jackpot' not in st.session_state:
     st.session_state.jackpot = random.randint(1, 100)
-    st.session_state.count = 5# 0
-    st.session_state.game_over = True #False
+    st.session_state.count = 0
+    st.session_state.game_over = False
 
 name = st.text_input("Enter your name:")
 guess = st.number_input("Guess the jackpot number between 1 and 100:", min_value=1, max_value=100, step=1)
